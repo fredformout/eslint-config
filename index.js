d22d1b96f0b01dbdf3fe0df12d4b65256d775fc4
Alexandr Subbotin <kelionweb@gmail.com>
2017-11-21 16:27:31 +0400

    Initial eslint config

---
+++ index.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: [
+    'eslint-config-airbnb-base'
+  ].map(require.resolve),
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true
+  },
+  rules: {
+    strict: 0,
+    'max-len': [2, {
+      code: 120,
+      ignoreUrls: true
+    }],
+    semi: ['error', 'never'],
+    'linebreak-style': ['error', 'unix'],
+    'import/extensions': ['off', 'never'],
+    'import/prefer-default-export': ['off'],
+    'import/no-extraneous-dependencies': ['off'],
+    'import/no-unresolved': 0,
+    'no-plusplus': ['off'],
+    'consistent-return': ['off'],
+  }
+};
